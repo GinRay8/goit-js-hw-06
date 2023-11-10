9cfe49ca7f767352ef3ef5d2856aa2c4667fdf71
GinRay8 <136614240+GinRay8@users.noreply.github.com>
2023-11-10 23:43:12 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const liElements = ingredients.map(ingredient => {
+  const liElement = document.createElement('li');
+  liElement.classList.add('item');
+  liElement.textContent = `${ingredient}`;
+
+  return liElement;
+});
+
+list.append(...liElements);

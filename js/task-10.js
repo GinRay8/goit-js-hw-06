8ff637008ee5bebc6c0e51fce9631b5cb2c6e323
GinRay8 <136614240+GinRay8@users.noreply.github.com>
2023-11-11 01:33:02 +0200

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const createButton = controls.querySelector("button[data-create]");
+const destroyButton = controls.querySelector("button[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createButton.addEventListener("click", createBox);
+destroyButton.addEventListener("click", destroyBox);
+
+function createBox() {
+  const numberOfBoxes = input.value;
+  let sizeBox = 30;
+
+  for (let i = 0; i < numberOfBoxes; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = sizeBox + "px";
+    box.style.height = sizeBox + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    sizeBox += 10;
+  }
+}
+
+function destroyBox() {
+  boxesContainer.innerHTML = "";
+}
